15f9d06db31bee690d206985da58990d078986f3
msantos98 <maria@frogames.es>
2021-10-11 16:40:48 +0200

    11 octubre 2021

--- Tema 2 DEIM/Assets/Scripts/MyFirstScript.cs
+++ Tema 2 DEIM/Assets/Scripts/MyFirstScript.cs
@@ -7,10 +7,12 @@ public class MyFirstScript : MonoBehaviour
 {
      /*
     // playerAge guarda edad del jugador
-    public int playerAge = 35;
+    
     private float playerSpeed = 5.25f;
     [SerializeField]  private bool gameOver;
-    */
+    
+     
+     
      
      public string playerName = "Maria";
      public string enemyName = "Maria ";
@@ -18,13 +20,24 @@ public class MyFirstScript : MonoBehaviour
      [SerializeField] private int x = 5;
      [SerializeField] private int y = 3;
      [SerializeField] private int z = 3;
+     */
+     
+     public int playerAge = 15;
 
-     [SerializeField] private bool isRaining; 
+     [SerializeField] private bool isRaining;
+     [SerializeField] private bool isCold;
+     
+     // [SerializeField] private int playerHP = 10;
     
     
     // Start is called before the first frame update
     void Start()
     {
+        if (playerAge % 4 == 0)
+        {
+            
+        }
+        
         /*
        Debug.Log(playerAge); 
        Debug.Log($"Hola {playerName}!");
@@ -34,7 +47,7 @@ public class MyFirstScript : MonoBehaviour
         Debug.Log("Resta: " + x + " - " + y + " = " + (x - y));
         Debug.Log(string.Format("Producto: {0} * {1} = {2}", x, y, x * y));
         Debug.Log(string.Format("Division: {0} / {1} = {2}", x, y, x / y));
-        */
+        
         if (playerName == enemyName)
         {
             Debug.Log("Player y enemigo se llaman igual");
@@ -45,18 +58,55 @@ public class MyFirstScript : MonoBehaviour
             Debug.Log("Player y enemigo tienen distinto nombre");
         }
         
+        
+        if (playerHP > 0)
+        {
+            Debug.Log("Sigues vivo");
+        }
+        else
+        {
+            Debug.Log("Estas muerto.");
+        }
+        
+
+        if (playerAge < 13)
+        {
+            Debug.Log("Eres un niño");
+        }
+        else if (playerAge < 18)
+        {
+            Debug.Log("Eres adolescente");
+        }
+        else
+        {
+            Debug.Log("Eres adulto");
+        }
+        */
+        
+        
     }
 
     // Update is called once per frame
     
     void Update()
     {
-        /*
-        if (isRaining)
+        
+        if (isRaining == true)
         {
-            Debug.Log("Lleva un paraguas");
+            if (isCold)
+            {
+                Debug.Log("Lleva un paraguas y lleva una sudadera");
+            }
+            else
+            {
+                Debug.Log("Lleva un paraguas");
+            }
         }
-        */
+        else
+        {
+            Debug.Log("No llueve");
+        }
+        
     }
     
 }
