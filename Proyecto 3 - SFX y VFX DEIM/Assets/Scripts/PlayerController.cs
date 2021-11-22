4d3ca8ad8042597ebb3dd978ec8a69d49efc2db4
msantos98 <maria@frogames.es>
2021-11-22 13:48:23 +0100

    22 nov

--- Proyecto 3 - SFX y VFX DEIM/Assets/Scripts/PlayerController.cs
+++ Proyecto 3 - SFX y VFX DEIM/Assets/Scripts/PlayerController.cs
@@ -2,44 +2,72 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRigidbody;
     private Animator playerAnimator;
+    private AudioSource playerAudioSource;
+    private AudioSource cameraAudioSource;
     public float jumpForce = 500;
     public float gravityModifier = 1;
     public bool isOnTheGround = true;
     public bool gameOver;
+    public ParticleSystem explosionParticleSystem;
+    public ParticleSystem dirtParticleSystem;
+    public AudioClip jumpClip;
+    public AudioClip explosionClip;
     
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        playerAudioSource = GetComponent<AudioSource>();
+        cameraAudioSource = GameObject.Find("Main Camera").GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
     }
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround && !gameOver)
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, 
                 ForceMode.Impulse);
             playerAnimator.SetTrigger("Jump_trig");
+            
+            dirtParticleSystem.Stop();
+            
+            playerAudioSource.PlayOneShot(jumpClip, 1);
             isOnTheGround = false;
         }
     }
 
     private void OnCollisionEnter(Collision otherCollider)
     {
-        if (otherCollider.gameObject.CompareTag("Ground"))
+        if (!gameOver)
         {
-            isOnTheGround = true;
-        } else if (otherCollider.gameObject.CompareTag("Obstacle"))
-        {
-            Debug.Log("GAME OVER");
-            gameOver = true;
+            if (otherCollider.gameObject.CompareTag("Ground"))
+            {
+                dirtParticleSystem.Play();
+                isOnTheGround = true;
+            }
+            else if (otherCollider.gameObject.CompareTag("Obstacle"))
+            {
+                int randomDeathType = Random.Range(1, 3);
+                playerAnimator.SetBool("Death_b", true);
+                playerAnimator.SetInteger("DeathType_int",
+                    randomDeathType);
+
+                explosionParticleSystem.Play();
+                dirtParticleSystem.Stop();
+                
+                cameraAudioSource.volume = 0.01f;
+                playerAudioSource.PlayOneShot(explosionClip, 1);
+
+                gameOver = true;
+            }
         }
-        
+
     }
 }
